e3ef96b86450992119973bd7e31b32f9cecc3ac1
Blackfianit <divashinin@mail.ru>
2021-08-25 13:22:27 +0300

    Created segue and new VC

---
+++ LogInApp/LoggedInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoggedInViewController.swift
+//  LogInApp
+//
+//  Created by Дмитрий Ивашинин on 25.08.2021.
+//
+
+import UIKit
+
+class LoggedInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
